7e612ab5622c73bf5ba7432d491a5ef8feac3d9f
Rafal Lesnicki <lesnickirafal@gmail.com>
2021-01-19 12:09:05 +0100

    first

---
+++ src/main/java/pl/javastart/bootcamp/lekcia21zadaniespringmvcprodukty/Lekcia21ZadanieSpringMvcProduktyApplication.java
@@ -0,0 +1,13 @@
+package pl.javastart.bootcamp.lekcia21zadaniespringmvcprodukty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lekcia21ZadanieSpringMvcProduktyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lekcia21ZadanieSpringMvcProduktyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/javastart/bootcamp/lekcia21zadaniespringmvcprodukty/Lekcia21ZadanieSpringMvcProduktyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.bootcamp.lekcia21zadaniespringmvcprodukty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lekcia21ZadanieSpringMvcProduktyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
